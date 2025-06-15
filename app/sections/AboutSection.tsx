986247a2581022733efd0114b643572abb5ad12e
Vrigz Alejo <brigsalejo@gmail.com>
2025-06-15 15:03:50 -0700

    Updated about me

--- app/sections/AboutSection.tsx
+++ app/sections/AboutSection.tsx
@@ -17,12 +17,14 @@ export default function AboutSection() {
                     <WaveText title={title} className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl pb-2 mb-8 sm:mb-12 lg:mb-16 font-bold" />
                     <div className="grid md:grid-cols-2 gap-12 items-center">
                         <div className="bg-white/10 dark:bg-gray-900/90 backdrop-blur-md border border-white/20 dark:border-gray-700/50 shadow-lg p-8 rounded-2xl">
-                            <h3 className="text-gray-900 dark:text-white text-2xl font-semibold mb-6">Full-Stack Web Developer</h3>
+                            <h3 className="text-gray-900 dark:text-white text-2xl font-semibold mb-6">Full-Stack Developer</h3>
                             <p className="text-gray-900 dark:text-white mb-6 leading-relaxed">
-                                I&apos;m passionate about creating seamless, user-friendly web applications that solve real-world problems. With a decade of experience as a Full-Stack Web Developer, I specialize in front-end and back-end technologies, crafting clean, efficient, and scalable solutions. Constantly learning and adapting to the latest trends, I thrive on building intuitive digital experiences that exceed expectations.
+                                I&apos;m a Full-Stack Developer with over 10 years of experience building and maintaining scalable web applications, APIs, and backend systems using both monolithic and microservices architectures. Skilled in PHP, Python, Node.js, TypeScript, JavaScript, SQL, NoSQL, and cloud platforms.
+                                Proven track record in improving system performance and reducing deployment issues by up to 80%. Experienced in using Generative AI tools to automate tasks, enhance user experience, and speed up development and debugging. Strong background in DevOps, containerization, and CI/CD processes.
+                                Committed to writing clean, efficient code and mentoring teams to adopt modern, AI-powered development practices that support long-term growth and innovation.
                             </p>
                             <p className="text-gray-900 dark:text-white leading-relaxed mb-8">
-                                When I&apos;m not coding, you&apos;ll find me exploring new technologies, vlogging, playing games, contributing to open-source projects, or creating exciting projects.
+                                When I&apos;m on free time, you&apos;ll find me taking care of my family, exploring new technologies, travelling, vlogging, playing games, contributing to community, contributing to open-source projects, or creating exciting projects.
                             </p>
                         </div>
                         <div className="relative">
